ea8e65722846e734840e931827efe321b2b197b3
LailtonSoares <lailsoares@hotmail.com>
2023-01-17 20:58:37 -0300

    my first commit

---
+++ src/data/words.js
@@ -0,0 +1,9 @@
+export const wordsList ={
+    carro:["Motor","Porta","Capô","Pneu","Antena"],
+    fruto:["Banana","Maçã","Pêra","Mamão","Laranja"],
+    corpo:["Braço","Perna","Cêrebro","Pescoço","Olhos"],
+    computador:["Mouse","Teclado","Monitor","Gabinete"],
+    carro:["Motor","Porta","Capô","Pneu","Antena"],
+    programação:["Liguagem","Framework","JavaScript","React"],
+    alimento:["Arroz","Feijão","Carne","Leite","Ovo"],
+}
